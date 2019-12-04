9389ba03ad0516c4c77ae62d1b187098dec7e258
Mauricio Cousillas <mauricio.cousillas@rootstrap.com>
2019-12-04 13:45:08 -0300

    Initial Commit

---
+++ todolist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todolist
+//
+//  Created by Mauricio Cousillas on 12/4/19.
+//  Copyright © 2019 Mauricio Cousillas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
